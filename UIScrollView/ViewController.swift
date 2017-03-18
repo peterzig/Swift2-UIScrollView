3369d2e8e2521b1ab564fb3276fe7dbe07c553b6
Peter Zaporowski <peter@Peters-Mac.local>
2017-03-18 01:34:02 +0100

    Simple UIScrollView through an array of iOS 7 icons

--- UIScrollView/ViewController.swift
+++ UIScrollView/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let WIDTH: CGFloat = 90 //width of the ImageView
+    let HEIGHT: CGFloat = 90 //height of an ImageView
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        for var x = 1; x <= 8; x++ {
+            let img = UIImage(named: "\(x)")
+            let imgView = UIImageView(image: img)
+            
+            scrollView.addSubview(imgView)
+            
+            imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(x)), 500, WIDTH, HEIGHT)
+        }
+        scrollView.contentSize = CGSizeMake( WIDTH * 10.5, scrollView.frame.size.height)
+        
+        // Width times 10.5 because we need more space for scrolling 8 ImageViews, Width times 8 cuts the last element of array
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
